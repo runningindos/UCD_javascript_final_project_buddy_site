2955a4a70ed60705c230dee7ac72ca17bb9724db
runningindos <56303584+runningindos@users.noreply.github.com>
2023-07-23 17:56:19 +0100

    created JS code for drop down, and added styling to the dropdown element.

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,4 @@
-//event listener for clicking on nav links
-//addEventListener("click", "");
-//event listener that scrolls up the height of the nav bar
-//addEventListener("scroll", "");
-
-//function showActiveLink()
 //remove and add active class to selected link
-//document.addEventListener('DOMContentLoaded', function() {
 let activeLink = document.querySelectorAll('.navLinks').forEach((navLink) => {
   navLink.addEventListener("click", (event) => {
     console.log(event.target);
@@ -15,47 +8,38 @@ let activeLink = document.querySelectorAll('.navLinks').forEach((navLink) => {
     
     //navLinks.classList.remove('active');
   navLink.classList.add('active');
+
  });
   
 
 });
-//});
-/*
-document.querySelectorAll(".navLinks").forEach(links => {
-  console.log(window.location);
-})
 
 
-
-//reveal Buddy's intro from the dropdown button beneath the "Read Buddy's Intro" banner//
-/*function aboutBuddyDropdownElement() {
-//1. create new div element//
-  let addIntroContainer = document.createElement("div");
-
-//2. add content to new div element//
-  let textNode = document.createTextNode("Here is where I'll go");
-
-//3. add content to new div element//
-  addIntroContainer.appendChild(textNode);
-
-//4. add new div element to the page//
-  let introBannerDiv = document.querySelector (".introBanner");
-introBannerDiv.appendChild(addIntroContainer);
-
-//5. 
-}*/
-
-
-function aboutBuddyDropdownElement2() {
-  document.getElementById("buddyIntroDiv").style.display = "flex"; 
+function signUpSuccessfull(){
+  let 
 }
-
-//nav buttons//
-//onclick to give the navigation the same hover styling when clicked and active - add event handler that uses specific styling when button is clicked//
-
-//apply class to active link//
-
-//remove class from inactive links//
-
-//make page scroll up the height of the nav banner//
-
+//Reveal/hide Buddy's Bio upon clicking down arrow
+let buddyIntroDiv = document.getElementById("buddyIntroDiv");
+let display = 0;
+let changeArrow = document.getElementById('downTab')
+function aboutBuddyDropdownElement2() {
+  if(display == 1) {
+    buddyIntroDiv.style.display = "block";
+    display = 0;
+  }
+  else
+  {
+    buddyIntroDiv.style.display = "none";
+    display = 1;
+  }
+} 
+//Change down arrow to up arrow when Buddy's bio is revealed
+function changeToUpArrow(){
+  let changeArrow = document.getElementById('downTab')
+  if(changeArrow.src.match('images/down-arrow.png')){
+    changeArrow.src = 'images/up-arrow.png'
+  } else {
+    changeArrow.src = 'images/down-arrow.png'
+  } 
+}
+//add divs to display faves
