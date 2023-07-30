bcbafcc82b28995394da0ece40fb09c48d3a2132
runningindos <56303584+runningindos@users.noreply.github.com>
2023-07-30 21:31:30 +0100

    Added update countdown very painstakingly, and it seems to work well

--- javascript/script.js
+++ javascript/script.js
@@ -12,11 +12,6 @@ let activeLink = document.querySelectorAll('.navLinks').forEach((navLink) => {
  });
 });
 
-
-function signUpSuccessful(){
-  let 
-}
-
 //Reveal/hide Buddy's Bio upon clicking down arrow
 let buddyIntroDiv = document.getElementById("buddyIntroDiv");
 let display = 0;
@@ -72,6 +67,28 @@ function displayFavesToys() {
     }
 }
 
+//establish countdown from speific date
+const countdown = () => {
+const eventDate = new Date('11/23/2023 00:00:00').getTime();
+const now = new Date()
+const difference = eventDate - now;
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const dayText = Math.floor(difference / day);
+const hourText = Math.floor((difference % day) / hour);
+const minuteText = Math.floor((difference % hour) / minute);
+const secondText = Math.floor((difference % minute) / second);
+
+//countdown event Listeners
+  document.querySelector('.days').innerText = dayText;
+  document.querySelector('.hours').innerText = hourText;
+  document.querySelector('.minutes').innerText = minuteText;
+  document.querySelector('.seconds').innerText = secondText;
+};
+setInterval(countdown, 1000);
+
 //form validation
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -89,9 +106,10 @@ function validateName() {
 
   //confirm name field length
   if (trimmedInput.length >= 2) {
-    document.getElementById("nameCriteria").style.color = "#2ab7ca";
+    document.getElementById("nameCriteriaMet").style.color = "#2ab7ca";
   } else {
     document.getElementById("nameCriteria").style.color = "initial"; // Reset the color if length is less than 2
   }
 }
 });
+
